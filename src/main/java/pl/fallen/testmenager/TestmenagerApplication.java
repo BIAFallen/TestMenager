c68289ce6cc97a0b032972ecb6be17a03e17ea9d
Your Name <you@example.com>
2018-02-20 18:29:31 +0100

    import project

---
+++ src/main/java/pl/fallen/testmenager/TestmenagerApplication.java
@@ -0,0 +1,12 @@
+package pl.fallen.testmenager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestmenagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestmenagerApplication.class, args);
+	}
+}
